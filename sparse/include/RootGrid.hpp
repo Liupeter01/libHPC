2f432cde7d45760c037575f6c6ff657c9320dcb1
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-05-01 13:34:14 +1000

    1.[ADD]:add rootgrid code with template layout support 2.[ADD]:support to combines all subblock as plugin

---
+++ sparse/include/RootGrid.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef _ROOTGRID_HPP_
+#define _ROOTGRID_HPP_
+
+namespace sparse {
+          template<typename _Ty, typename _Layout>
+          struct RootGrid {
+
+                    _Layout m_root;
+          };
+}
+
+
+
+#endif //_ROOTGRID_HPP_
---
+++ sparse/src/RootGrid.cpp
@@ -0,0 +1,1 @@
+#include <RootGrid.hpp>
