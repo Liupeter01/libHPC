473508218cd3410b8a60571479703d1eb18f5991
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-05-03 20:21:59 +1000

    1.[UPDATE]:update overrided function with the newest def in base CRTP class 2.[ADD]:add getTransferredCoord as command interface

--- sparse/include/DenseBlock.hpp
+++ sparse/include/DenseBlock.hpp
@@ -1,28 +1,37 @@
 #pragma once
-#include <iterator>
-#include <optional>
 #ifndef _DENSEBLOCK_HPP_
 #define _DENSEBLOCK_HPP_
 #include <BaseBlock.hpp>
 
 namespace sparse {
-
+                    namespace details {
+                    using Coord2D = std::pair<std::intptr_t, std::intptr_t>;
+          } // namespace details
 template <std::intptr_t BlockSize, typename _Ty>
-struct DenseBlock : BaseBlock<BlockSize, true, _Ty> {
+struct DenseBlock : BlockInfo<BlockSize, true, _Ty> {
+
   static_assert((BlockSize & (BlockSize - 1)) == 0,
                 "BlockSize must be a power of 2");
 
-  using BaseType = ::sparse::BaseBlock<BlockSize, true, _Ty>;
+  using value_type = _Ty;
+  using pointer = std::add_pointer_t<_Ty>;
+  using reference = _Ty&;
+  using const_reference = const reference;
 
-  virtual std::optional<_Ty> operator()(const std::intptr_t x,
-                                        const std::intptr_t y) const override;
+  virtual std::optional<reference> operator()(const std::intptr_t x,
+                                                                  const std::intptr_t y) override;
 
-  virtual const _Ty &read(const std::intptr_t x,
-                          const std::intptr_t y) const override;
-  virtual _Ty *fetch(const std::intptr_t x,
-                     const std::intptr_t y) const override;
+  virtual  std::optional<const  _Ty&>read(const std::intptr_t x,
+                                                                  const std::intptr_t y) const override;
+
   virtual void write(const std::intptr_t x, const std::intptr_t y,
                      const _Ty &value) override;
+
+  virtual std::optional < std::reference_wrapper<value_type>>
+            fetch_pointer(const std::intptr_t x, const std::intptr_t y) const override;
+
+  virtual  std::reference_wrapper<value_type>
+            touch_pointer(const std::intptr_t x, const std::intptr_t y) override;
 
   template <typename Func> void foreach (Func &&func) {
     for (std::size_t x = 0; x < BlockSize; ++x) {
@@ -33,6 +42,9 @@ struct DenseBlock : BaseBlock<BlockSize, true, _Ty> {
   }
 
   _Ty m_block[BlockSize][BlockSize];
+
+private:
+          details::Coord2D getTransferredCoord(const std::intptr_t x, const std::intptr_t y);
 };
 } // namespace sparse
 
--- sparse/src/DenseBlock.cpp
+++ sparse/src/DenseBlock.cpp
@@ -2,25 +2,33 @@
 #include <DenseBlock.hpp>
 
 template <std::intptr_t BlockSize, typename _Ty>
-std::optional<_Ty>
+std::optional<_Ty&>
 sparse::DenseBlock<BlockSize, _Ty>::operator()(const std::intptr_t x,
-                                               const std::intptr_t y) const {
-  return m_block[x & BaseType::BMask][y & BaseType::BMask];
+                                               const std::intptr_t y) {
+
+          auto [new_x, new_y] = getTransferredCoord(x, y);
+          return m_block[new_x][new_y];
 }
 
 template <std::intptr_t BlockSize, typename _Ty>
-_Ty *sparse::DenseBlock<BlockSize, _Ty>::fetch(const std::intptr_t x,
-                                               const std::intptr_t y) const {
-
-  return &m_block[x & BaseType::BMask][y & BaseType::BMask];
-}
-
-template <std::intptr_t BlockSize, typename _Ty>
-const _Ty &
+std::optional<const _Ty&>
 sparse::DenseBlock<BlockSize, _Ty>::read(const std::intptr_t x,
-                                         const std::intptr_t y) const {
+          const std::intptr_t y) const {
 
-  return m_block[x & BaseType::BMask][y & BaseType::BMask];
+          return operator()(x, y);
+}
+
+template <std::intptr_t BlockSize, typename _Ty>
+std::optional <  std::reference_wrapper<_Ty>>
+sparse::DenseBlock<BlockSize, _Ty>::fetch_pointer(const std::intptr_t x, const std::intptr_t y) const {
+          auto [new_x, new_y] = getTransferredCoord(x, y);
+          return { &m_block[new_x][new_y] };
+}
+
+template <std::intptr_t BlockSize, typename _Ty>
+std::reference_wrapper<_Ty>
+sparse::DenseBlock<BlockSize, _Ty>::touch_pointer(const std::intptr_t x, const std::intptr_t y) {
+          return  fetch_pointer(x, y);
 }
 
 template <std::intptr_t BlockSize, typename _Ty>
@@ -28,5 +36,14 @@ void sparse::DenseBlock<BlockSize, _Ty>::write(const std::intptr_t x,
                                                const std::intptr_t y,
                                                const _Ty &value) {
 
-  m_block[x & BaseType::BMask][y & BaseType::BMask] = value;
+          auto ref_opt = operator()(x, y);
+          if (ref_opt) {
+                    *ref_opt = value;
+          }
+}
+
+template <std::intptr_t BlockSize, typename _Ty>
+sparse::details::Coord2D
+sparse::DenseBlock<BlockSize, _Ty>::getTransferredCoord(const std::intptr_t x, const std::intptr_t y) {
+          return std::make_pair(x & this->BMask, y & this->BMask);
 }
