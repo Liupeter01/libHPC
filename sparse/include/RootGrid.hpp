dba10d506d602baeddfa9a724b7987add5f23ee7
Liupeter01 <Liupeter012001@outlook.com>
2025-05-04 15:27:19 +1000

    1.[REMOVE]:remove source file due to the error of linker

--- sparse/include/RootGrid.hpp
+++ sparse/include/RootGrid.hpp
@@ -1,37 +1,67 @@
 #pragma once
 #ifndef _ROOTGRID_HPP_
 #define _ROOTGRID_HPP_
-#include<iostream>
+#include <iostream>
 #include <optional>
 
 namespace sparse {
-          namespace details {
-                    using Coord2D = std::pair<std::intptr_t, std::intptr_t>;
-          } // namespace details
+namespace details {
+using Coord2D = std::pair<std::intptr_t, std::intptr_t>;
+} // namespace details
 
-template <typename _Ty, typename _Layout> 
-struct RootGrid {
-          
-          template<typename Node>
-          static  std::optional<_Ty> _read(const Node& node, std::intptr_t x, std::intptr_t y);
+template <typename _Ty, typename _Layout> struct RootGrid {
 
-          template<typename Node>
-          static void _write(Node& node, std::intptr_t x, std::intptr_t y, const _Ty& value);
+  template <typename Node>
+  static std::optional<_Ty> _read(const Node &node, std::intptr_t x,
+                                  std::intptr_t y) {
+    // if it is denseblock, then it should be the last node point!
+    if constexpr (Node::is_leaf) {
+      return node.read(x, y);
+    } else {
+      const auto [new_x, new_y] = generateCoord<Node>(x, y);
+      /*if it is hash/pointer block, then find the next block and do
+       * recursion!*/
+      auto next_node = node.read(new_x, new_y);
 
-          std::optional<_Ty> read(std::intptr_t x, std::intptr_t y) const;
-          void write(std::intptr_t x, std::intptr_t y, const _Ty& value);
+      if (!next_node.has_value())
+        return std::nullopt;
+      // you have to pass global coordinates to the function!
+      return _read(next_node.value(), x, y);
+    }
+  }
 
-          template<typename Func>
-          void foreach(Func&& func);
+  template <typename Node>
+  static void _write(Node &node, std::intptr_t x, std::intptr_t y,
+                     const _Ty &value) {
+    // if it is denseblock, then it should be the last node point!
+    if constexpr (Node::is_leaf) {
+      node.write(x, y, value);
+    } else {
+      const auto [new_x, new_y] = generateCoord<Node>(x, y);
+      /*if it is hash/pointer block, then find the next block and do
+       * recursion!*/
+      auto &next_node = node.touch_pointer(new_x, new_y);
+      _write(next_node.get(), x, y, value);
+    }
+  }
 
-  _Layout m_root;   //maybe start from hashmap
+  std::optional<_Ty> read(std::intptr_t x, std::intptr_t y) const {
+    return _read(m_root, x, y);
+  }
+  void write(std::intptr_t x, std::intptr_t y, const _Ty &value) {
+    _write(m_root, x, y, value);
+  }
+
+  template <typename Func> void foreach (Func &&func);
+
+  _Layout m_root; // maybe start from hashmap
 
 private:
-          template<typename Node>
-          details::Coord2D generateCoord(const std::intptr_t x, const std::intptr_t y) {
-                    return std::make_pair(x >> Node::subblock_shift_bits,
-                                                        y >> Node::subblock_shift_bits);
-          }
+  template <typename Node>
+  details::Coord2D generateCoord(const std::intptr_t x, const std::intptr_t y) {
+    return std::make_pair(x >> Node::subblock_shift_bits,
+                          y >> Node::subblock_shift_bits);
+  }
 };
 } // namespace sparse
 
