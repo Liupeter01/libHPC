db74da4cdfe2e75214a2d0d7447bd1fc64b8b7b5
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-05-04 11:01:24 +1000

    1.[ADD]:add read and write interface which based on if constexpr recursive method 2.[DESIGN]:seperate generatecoord for abstract purposes 3.[NOT IMPL]:for each

--- sparse/include/RootGrid.hpp
+++ sparse/include/RootGrid.hpp
@@ -1,11 +1,37 @@
 #pragma once
 #ifndef _ROOTGRID_HPP_
 #define _ROOTGRID_HPP_
+#include<iostream>
+#include <optional>
 
 namespace sparse {
-template <typename _Ty, typename _Layout> struct RootGrid {
+          namespace details {
+                    using Coord2D = std::pair<std::intptr_t, std::intptr_t>;
+          } // namespace details
 
-  _Layout m_root;
+template <typename _Ty, typename _Layout> 
+struct RootGrid {
+          
+          template<typename Node>
+          static  std::optional<_Ty> _read(const Node& node, std::intptr_t x, std::intptr_t y);
+
+          template<typename Node>
+          static void _write(Node& node, std::intptr_t x, std::intptr_t y, const _Ty& value);
+
+          std::optional<_Ty> read(std::intptr_t x, std::intptr_t y) const;
+          void write(std::intptr_t x, std::intptr_t y, const _Ty& value);
+
+          template<typename Func>
+          void foreach(Func&& func);
+
+  _Layout m_root;   //maybe start from hashmap
+
+private:
+          template<typename Node>
+          details::Coord2D generateCoord(const std::intptr_t x, const std::intptr_t y) {
+                    return std::make_pair(x >> Node::subblock_shift_bits,
+                                                        y >> Node::subblock_shift_bits);
+          }
 };
 } // namespace sparse
 
