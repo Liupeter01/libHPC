fac3b51ac716bab281a18a1787b1ec9bcd339a22
Liupeter01 <Liupeter012001@outlook.com>
2025-05-04 15:29:14 +1000

    1.[ADD]:add dummy file for cmakefile and compile system to generate lib file

---
+++ sparse/src/dummy.cpp
@@ -0,0 +1,5 @@
+#include <SparseDS.hpp>
+
+// This source file is only a placeholder to ensure the sparse_data_structure static library
+// is built.
+void __ensure_sparse_data_structure_compiles() {}
