1a4a0989ab1b5426d9358f0fcabbae1265591b2d
Liupeter01 <Liupeter012001@outlook.com>
2025-05-04 15:09:08 +1000

    1.[REMOVE]:remove source file due to the error of linker

--- sparse/include/HashBlock.hpp
+++ sparse/include/HashBlock.hpp
@@ -15,50 +15,90 @@ static void hash_combine_impl(SizeT &seed, SizeT value) {
 } // namespace details
 
 template <typename OtherBlock>
-struct HashBlock 
-          :public BlockInfo<1, false, OtherBlock>       /*only related to current block, which is hashblock*/
+struct HashBlock
+    : public BlockInfo<1, false, OtherBlock> /*only related to current block,
+                                                which is hashblock*/
 {
-          using value_type = OtherBlock;
-          using pointer = std::add_pointer_t<OtherBlock>;
-          using reference = OtherBlock&;
-          using const_reference = const reference;
-          using CurrBlockType = BlockInfo<1, false, OtherBlock>;
+  using value_type = OtherBlock;
+  using pointer = std::add_pointer_t<OtherBlock>;
+  using reference = OtherBlock &;
+  using const_reference = const reference;
+  using CurrBlockType = BlockInfo<1, false, OtherBlock>;
 
-          /*related to subblock, we need its offset argument*/
-          static constexpr std::intptr_t subblock_shift_bits = SubBlockInfo<OtherBlock>::offset_bits;
+  /*related to subblock, we need its offset argument*/
+  static constexpr std::intptr_t subblock_shift_bits =
+      SubBlockInfo<OtherBlock>::offset_bits;
 
-  virtual std::optional< reference> operator()(const std::intptr_t x,
-                                                                                const std::intptr_t y) override;
-
-  virtual std::optional<const OtherBlock&> operator()(const std::intptr_t x,
-            const std::intptr_t y) const override;
-
-  virtual  std::optional<const OtherBlock&>read(const std::intptr_t x,
-                                                                                const std::intptr_t y) const override;
-
-  virtual void write(const std::intptr_t x, 
-                               const std::intptr_t y,
-                                const OtherBlock& value) override;
-
-  virtual std::optional < std::reference_wrapper<value_type>> fetch_pointer(const std::intptr_t x,
-                                                                                                                            const std::intptr_t y) const override;
-
-  virtual std::reference_wrapper<value_type> touch_pointer(const std::intptr_t x,
-                                                                                                    const std::intptr_t y) override;
-
-  template <typename Func> 
-  void foreach (Func &&func) {
-            for (auto& [key, value] : m_data) {
-                      func(key.first, key.second, *value);
-          }
+  virtual std::optional<reference> operator()(const std::intptr_t x,
+                                              const std::intptr_t y) override {
+    auto key = getKey(x, y);
+    auto it = m_data.find(key);
+    if (it == m_data.end()) {
+      return std::nullopt;
+    }
+    return *it->second;
   }
 
-  std::unordered_map<details::Coord2D, std::unique_ptr<OtherBlock>, std::hash<details::Coord2D>>
+  virtual std::optional<const OtherBlock &>
+  operator()(const std::intptr_t x, const std::intptr_t y) const override {
+    auto key = getKey(x, y);
+    auto it = m_data.find(key);
+    if (it == m_data.end()) {
+      return std::nullopt;
+    }
+    return *it->second;
+  }
+
+  virtual std::optional<const OtherBlock &>
+  read(const std::intptr_t x, const std::intptr_t y) const override {
+    return operator()(x, y);
+  }
+
+  virtual void write(const std::intptr_t x, const std::intptr_t y,
+                     const OtherBlock &value) override {
+    touch_pointer(x, y).get() = value;
+  }
+
+  virtual std::optional<std::reference_wrapper<value_type>>
+  fetch_pointer(const std::intptr_t x, const std::intptr_t y) const override {
+    auto key = getKey(x, y);
+    auto it = m_data.find(key);
+    if (it == m_data.end()) {
+      return std::nullopt;
+    }
+    return {*it->second};
+  }
+
+  virtual std::reference_wrapper<value_type>
+  touch_pointer(const std::intptr_t x, const std::intptr_t y) override {
+    auto key = getKey(x, y);
+    auto it = m_data.find(key);
+    if (it != m_data.end()) {
+      return *it->second;
+    }
+    auto block = std::make_unique<OtherBlock>();
+    auto ptr = block.get();
+    m_data.emplace(key, std::move(block));
+    return {*ptr};
+  }
+
+  template <typename Func> void foreach (Func &&func) {
+    for (auto &[key, value] : m_data) {
+      func(key.first, key.second, *value);
+    }
+  }
+
+  std::unordered_map<details::Coord2D, std::unique_ptr<OtherBlock>,
+                     std::hash<details::Coord2D>>
       m_data;
 
 private:
-          details::Coord2D getKey(const std::intptr_t x, const std::intptr_t y) const;
-
+  details::Coord2D getKey(const std::intptr_t x, const std::intptr_t y) const {
+    return std::make_pair(x, y);
+    // Currently, we do not need to use this
+    // return std::make_pair(x >> this->subblock_shift_bits,
+    //                       y >> this->subblock_shift_bits);
+  }
 };
 } // namespace sparse
 
