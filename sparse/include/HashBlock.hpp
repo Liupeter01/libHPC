b85c186e82e05834d48d8d3a4d434ff478677099
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-05-06 19:48:54 +1000

    1.[UPDATE]:start to use concurrent_hash_map to achieve concurrent access 2.[ADD]:all write/read are done by using (const)accessor 3.[MODIFY]:modify foreach, we record all valid key values to a vector container and use find to operate them, IN CASE iterator failure happened

--- sparse/include/HashBlock.hpp
+++ sparse/include/HashBlock.hpp
@@ -4,6 +4,8 @@
 #include <BaseBlock.hpp>
 #include <iostream>
 #include <unordered_map>
+#include <tbb/concurrent_vector.h>
+#include <tbb/concurrent_hash_map.h>
 
 namespace sparse {
 namespace details {
@@ -12,6 +14,19 @@ template <typename SizeT>
 static void hash_combine_impl(SizeT &seed, SizeT value) {
   seed ^= value + 0x9e3779b9 + (seed << 6) + (seed >> 2);
 }
+
+struct Coord2D_HashCompare {
+          static size_t hash(const Coord2D& key) {
+                    std::size_t seed = 0;
+                    sparse::details::hash_combine_impl(seed, static_cast<std::size_t>(key.first));
+                    sparse::details::hash_combine_impl(seed, static_cast<std::size_t>(key.second));
+                    return seed;
+          }
+          static bool equal(const  details::Coord2D& a, const  details::Coord2D& b) {
+                    return a == b;
+          }
+};
+
 } // namespace details
 
 template <typename OtherBlock>
@@ -23,6 +38,13 @@ struct HashBlock
   using reference = OtherBlock &;
   using const_value = const OtherBlock;
   using CurrBlockType = BlockInfo<1, false, OtherBlock>;
+  using ContainerType = tbb::concurrent_hash_map<
+            details::Coord2D,
+            std::unique_ptr<OtherBlock>,
+            details::Coord2D_HashCompare>;
+
+  using ContainerAccessor = typename ContainerType::accessor;
+  using ConstContainerAccessor = typename ContainerType::const_accessor;
 
   /*related to subblock, we need its offset argument*/
   static constexpr std::intptr_t subblock_shift_bits =
@@ -31,21 +53,21 @@ struct HashBlock
   virtual std::optional<std::reference_wrapper<value_type>>
   operator()(const std::intptr_t x, const std::intptr_t y) override {
     auto key = getKey(x, y);
-    auto it = m_data.find(key);
-    if (it == m_data.end()) {
-      return std::nullopt;
+    ContainerAccessor accessor;
+    if (!m_data.find(accessor, key)) {
+              return std::nullopt;
     }
-    return std::make_optional(std::ref(*it->second));
+    return std::ref(*accessor->second);  // accessor acts like iterator
   }
 
   virtual std::optional<std::reference_wrapper<const_value>>
   operator()(const std::intptr_t x, const std::intptr_t y) const override {
-    auto key = getKey(x, y);
-    auto it = m_data.find(key);
-    if (it == m_data.end()) {
-      return std::nullopt;
-    }
-    return std::make_optional(std::cref(*it->second));
+            auto key = getKey(x, y);
+            ConstContainerAccessor accessor;
+  if (!m_data.find(accessor, key)) {
+    return std::nullopt;
+  }
+  return std::cref(*accessor->second);  
   }
 
   virtual std::optional<std::reference_wrapper<const_value>>
@@ -57,55 +79,51 @@ struct HashBlock
                      const OtherBlock &value) override {
     touch_pointer(x, y).get() = value;
   }
+  virtual void write(const std::intptr_t x, const std::intptr_t y,
+            OtherBlock&& value) override {
+            touch_pointer(x, y).get() = std::move(value);
+  }
 
   virtual std::optional<std::reference_wrapper<value_type>>
   fetch_pointer(const std::intptr_t x, const std::intptr_t y) override {
-    auto key = getKey(x, y);
-    auto it = m_data.find(key);
-    if (it == m_data.end()) {
-      return std::nullopt;
-    }
-    return std::make_optional(std::ref(*it->second));
+            return operator()(x, y);
   }
 
   virtual std::reference_wrapper<value_type>
   touch_pointer(const std::intptr_t x, const std::intptr_t y) override {
-    auto key = getKey(x, y);
-    auto &ptr = m_data[key];
-    if (!ptr)
-      ptr = std::make_unique<OtherBlock>();
-    return std::ref(*ptr);
+            auto key = getKey(x, y);
+            ContainerAccessor accessor;
+            m_data.insert(accessor, key);
+
+            if (!accessor->second) {
+                      accessor->second = std::make_unique<OtherBlock>();
+            }
+            return std::ref(*accessor->second);
   }
 
   template <typename Func> void foreach (Func &&func) {
-    for (auto &[key, value] : m_data) {
-      func(key.first, key.second, *value);
-    }
+            std::vector<details::Coord2D> keys;
+            keys.reserve(m_data.size());
+
+            for (auto it = m_data.begin(); it != m_data.end(); ++it) {
+                      keys.push_back(it->first);
+            }
+            auto size = keys.size();
+            tbb::parallel_for(std::size_t(0), size, [&](std::size_t i) {
+                      ContainerAccessor acc;
+                      if (m_data.find(acc, keys[i])) {
+                                func(acc->first.first, acc->first.second, *acc->second);
+                      }
+                      });
   }
 
-  std::unordered_map<details::Coord2D, std::unique_ptr<OtherBlock>,
-                     std::hash<details::Coord2D>>
-      m_data;
+  ContainerType m_data;
 
 private:
   details::Coord2D getKey(const std::intptr_t x, const std::intptr_t y) const {
     return std::make_pair(x, y);
-    // Currently, we do not need to use this
-    // return std::make_pair(x >> this->subblock_shift_bits,
-    //                       y >> this->subblock_shift_bits);
   }
 };
 } // namespace sparse
-
-namespace std {
-template <> struct std::hash<sparse::details::Coord2D> {
-  std::intptr_t operator()(const sparse::details::Coord2D &vertex) const {
-    std::intptr_t seed = 0;
-    sparse::details::hash_combine_impl(seed, vertex.first);
-    sparse::details::hash_combine_impl(seed, vertex.second);
-    return seed;
-  }
-};
-} // namespace std
 
 #endif // _HASHBLOCK_HPP_
