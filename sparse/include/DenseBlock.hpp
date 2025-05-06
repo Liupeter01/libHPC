8c7bfcc95acddd7778a9f0ffa6e55c54111b4a9b
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-05-06 19:46:58 +1000

    1.[UPDATE]:instead of using lock, i choose to use concurrent_vector container to secure the thread safty issue 2.[FEATURE]:flat array

--- sparse/include/DenseBlock.hpp
+++ sparse/include/DenseBlock.hpp
@@ -2,6 +2,7 @@
 #ifndef _DENSEBLOCK_HPP_
 #define _DENSEBLOCK_HPP_
 #include <BaseBlock.hpp>
+#include <tbb/concurrent_vector.h>
 #include <cassert>
 
 namespace sparse {
@@ -15,52 +16,61 @@ struct DenseBlock : BlockInfo<BlockSize, true, _Ty> {
                 "BlockSize must be a power of 2");
 
   using value_type = _Ty;
-  using reference = _Ty &;
+  using reference = _Ty&;
   using const_value = const _Ty;
 
-  virtual std::optional<std::reference_wrapper<value_type>>
-  operator()(const std::intptr_t x, const std::intptr_t y) override {
-    auto [new_x, new_y] = getTransferredCoord(x, y);
-    return std::make_optional(std::ref(m_block[new_x][new_y]));
+  DenseBlock() {
+            m_block.resize(BlockSize * BlockSize);
   }
 
-  virtual std::optional<std::reference_wrapper<const_value>>
-  operator()(const std::intptr_t x, const std::intptr_t y) const override {
-    auto [new_x, new_y] = getTransferredCoord(x, y);
-    return std::make_optional(std::ref(m_block[new_x][new_y]));
+  std::optional<std::reference_wrapper<value_type>>
+            operator()(const std::intptr_t x, const std::intptr_t y) override {
+            auto [new_x, new_y] = getTransferredCoord(x, y);
+            return std::make_optional(std::ref(m_block[new_x * BlockSize + new_y]));
   }
 
-  virtual std::optional<std::reference_wrapper<const_value>>
-  read(const std::intptr_t x, const std::intptr_t y) const override {
-    return operator()(x, y);
+  std::optional<std::reference_wrapper<const_value>>
+            operator()(const std::intptr_t x, const std::intptr_t y) const override {
+            auto [new_x, new_y] = getTransferredCoord(x, y);
+            return std::make_optional(std::cref(m_block[new_x * BlockSize + new_y]));
   }
 
-  virtual void write(const std::intptr_t x, const std::intptr_t y,
-                     const _Ty &value) override {
-    touch_pointer(x, y).get() = value;
+  std::optional<std::reference_wrapper<const_value>>
+            read(const std::intptr_t x, const std::intptr_t y) const override {
+            return operator()(x, y);
   }
 
-  virtual std::optional<std::reference_wrapper<value_type>>
-  fetch_pointer(const std::intptr_t x, const std::intptr_t y) override {
-    return operator()(x, y);
+  void write(const std::intptr_t x, const std::intptr_t y, const _Ty& value) override {
+            touch_pointer(x, y).get() = value;
   }
 
-  virtual std::reference_wrapper<value_type>
-  touch_pointer(const std::intptr_t x, const std::intptr_t y) override {
-    auto opt = fetch_pointer(x, y);
-    assert(opt.has_value() && "touch_pointer() called on invalid coordinate");
-    return *opt;
+  void write(const std::intptr_t x, const std::intptr_t y, _Ty&& value) override {
+            touch_pointer(x, y).get() = std::move(value);
   }
 
-  template <typename Func> void foreach (Func &&func) {
-    for (std::size_t x = 0; x < BlockSize; ++x) {
-      for (std::size_t y = 0; y < BlockSize; ++y) {
-        func(x, y, m_block[x][y]);
-      }
-    }
+  std::optional<std::reference_wrapper<value_type>>
+            fetch_pointer(const std::intptr_t x, const std::intptr_t y) override {
+            return operator()(x, y);
   }
 
-  _Ty m_block[BlockSize][BlockSize];
+  std::reference_wrapper<value_type>
+            touch_pointer(const std::intptr_t x, const std::intptr_t y) override {
+            auto opt = fetch_pointer(x, y);
+            assert(opt.has_value());
+            return *opt;
+  }
+
+  template <typename Func>
+  void foreach(Func&& func) {
+#pragma omp parallel for collapse(2)
+            for (std::size_t x = 0; x < BlockSize; ++x) {
+                      for (std::size_t y = 0; y < BlockSize; ++y) {
+                                func(x, y, m_block[x * BlockSize + y]);
+                      }
+            }
+  }
+
+  tbb::concurrent_vector<_Ty> m_block;
 
 private:
   details::Coord2D getTransferredCoord(const std::intptr_t x,
