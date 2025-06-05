083e1d81580ce55bfeb0d5ac45ac6ff60edac789
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-06-05 12:59:37 +1000

    1.[REMOVE]:remove pervious test 2.[ADD]:add hierarchical lock test

---
+++ tests/test_hierarchical_lock/test_hierarchical_lock_multithread.cc
@@ -0,0 +1,29 @@
+#include <thread>
+#include <gtest/gtest.h>
+#include <hierarchical_lock.hpp>
+
+TEST(HierarchicalLockTest, MultiThreadedLockOrder) {
+          hierarchical_lock lock1(1000);
+          hierarchical_lock lock2(500);
+
+          std::thread th1([&lock1, &lock2]() {
+                    EXPECT_NO_THROW({
+                    lock1.lock();
+                    lock2.lock();
+                    lock2.unlock();
+                    lock1.unlock();
+                              }, std::runtime_error);
+                    });
+
+          std::thread th2([&lock1, &lock2]() {
+                    EXPECT_THROW({
+                        lock2.lock();
+                        lock1.lock();
+                        lock1.unlock();
+                        lock2.unlock();
+                              }, std::runtime_error);
+                    });
+
+          th1.join();
+          th2.join();
+}
---
+++ tests/test_hierarchical_lock/test_hierarchical_lock_sequence.cc
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+#include <hierarchical_lock.hpp>
+
+TEST(HierarchicalLockTest, ValidLockOrder) {
+          hierarchical_lock lock1(1000);
+          hierarchical_lock lock2(500);
+
+          EXPECT_NO_THROW({
+              lock1.lock();
+              lock2.lock();
+              lock2.unlock();
+              lock1.unlock();
+                    });
+}
+
+TEST(HierarchicalLockTest, InvalidLockOrder) {
+          hierarchical_lock lock1(1000);
+          hierarchical_lock lock2(500);
+
+          //there should be a throw
+          EXPECT_THROW({
+              lock2.lock();
+              lock1.lock(); 
+              lock1.unlock();
+              lock2.unlock();
+                    }, std::runtime_error);
+}
