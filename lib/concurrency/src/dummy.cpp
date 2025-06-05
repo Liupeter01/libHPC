4440f4a6759b6f69048805718aa8d41d23ea1cef
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-06-05 12:58:57 +1000

    1.[ADD]:add hierarchical lock to manage if there are multiples lock instead one thread, and throw exception when the sequence is wrong

---
+++ lib/concurrency/src/dummy.cpp
@@ -0,0 +1,7 @@
+#include <hierarchical_lock.hpp>
+
+thread_local std::size_t hierarchical_lock::thread_level = std::numeric_limits<std::size_t>::max();
+
+// This source file is only a placeholder to ensure the hpc_array static library
+// is built.
+void __ensure_concurrency_compiles() {}
