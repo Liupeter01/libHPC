a20a8e7cf38d91fc8bf61d6234b9710ab624276b
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-06-10 23:15:12 +1000

    1.[ADD]:add circular queue based on mutex lock

---
+++ lib/concurrency/circular_queue/src/dummy.cpp
@@ -0,0 +1,6 @@
+#include <circular_queue_lk.hpp>
+#include<circular_queue_lockfree.hpp>
+
+// This source file is only a placeholder to ensure the hpc_array static library
+// is built.
+void __ensure_circular_queue_compiles() {}
