d0c795e3ae1e7e378e2aac99b1916cdb8a6d5a45
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-07-09 10:39:05 +1000

    1.[NOT TESTED!] 2.[ADD]:add stack with mutex

---
+++ lib/concurrency/stack/src/dummy.cpp
@@ -0,0 +1,6 @@
+#include <stack_lk.hpp>
+#include <stack_lockfree.hpp>
+
+// This source file is only a placeholder to ensure the hpc_array static library
+// is built.
+void __ensure_stack_compiles() {}
