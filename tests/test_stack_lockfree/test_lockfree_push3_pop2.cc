87f2351b97792cd17c725d8cace0ade11581c6d7
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-07-11 22:47:43 +1000

    1.[TESTED]:test lockfree stack with smart_ptr(pop) under (im)balanced push & pop condition 2.[POTENTIAL BUG]:unique_ptr refenerce counting error with #define NUMBER 2000000

---
+++ tests/test_stack_lockfree/test_lockfree_push3_pop2.cc
@@ -0,0 +1,57 @@
+#include <thread>
+#include <gtest/gtest.h>
+#include <stack_lockfree.hpp>
+
+#define NUMBER 2000000
+
+TEST(LockFreeStackTest, MultiThreadForPush3AndPop2) {
+
+          concurrency::ConcurrentStack<std::size_t> list;
+
+          auto producer = [&list]() {
+                    for (std::size_t i = 0; i < NUMBER; ++i) {
+                              list.push(i);
+                    }
+                    };
+
+          std::vector<std::thread> producer_list;
+          producer_list.emplace_back(producer);
+          producer_list.emplace_back(producer);
+          producer_list.emplace_back(producer);
+
+          std::thread th4([&list, producer_number = producer_list.size()]() {
+                    thread_local std::size_t popped = 0;
+                    while (popped < NUMBER * producer_number / 2) {
+                              auto val = list.pop();
+                              if (val.has_value()) {
+                                        ++popped;
+                                        continue;
+                              }
+                              std::this_thread::sleep_for(std::chrono::microseconds(1));
+                    }
+                    });
+
+          std::thread th5([&list, producer_number = producer_list.size()]() {
+                    thread_local std::size_t popped = 0;
+                    while (popped < NUMBER * producer_number / 2) {
+                              auto val = list.pop();
+                              if (val.has_value()) {
+                                        ++popped;
+                                        continue;
+                              }
+                              std::this_thread::sleep_for(std::chrono::microseconds(1));
+                    }
+                    });
+
+
+          th4.join();
+          th5.join();
+
+          for (auto& th : producer_list) {
+                    if (th.joinable()) {
+                              th.join();
+                    }
+          }
+
+          EXPECT_EQ(list.size(), 0);
+}
