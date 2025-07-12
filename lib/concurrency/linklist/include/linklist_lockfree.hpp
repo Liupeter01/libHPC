8df3a4cc89c4df8642dea57232e54248d01ea938
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-07-12 10:32:32 +1000

    1.[ADD]:add prototype file for compile

---
+++ lib/concurrency/linklist/include/linklist_lockfree.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef _LINKLIST_LOCKFREE_HPP_
+#define _LINKLIST_LOCKFREE_HPP_
+#include <memory>
+#include <atomic>
+
+namespace concurrency {
+          template<typename _Ty>
+          class ConcurrentLinkList;
+}
+
+#endif //LINKLIST_LOCKFREE_HPP_
---
+++ lib/concurrency/queue/include/queue_lockfree.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef _QUEUE_LOCKFREE_HPP
+#define _QUEUE_LOCKFREE_HPP
+#include <memory>
+#include <atomic>
+
+namespace concurrency {
+          template<typename _Ty>
+          class ConcurrentQueue;
+}
+
+#endif // _QUEUE_LOCKFREE_HPP
