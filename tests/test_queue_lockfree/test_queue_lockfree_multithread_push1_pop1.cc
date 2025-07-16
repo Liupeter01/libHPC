fd92c9657bd1d1bfabe07227f9ab62ddaaf10c5d
Liupeter01 <Liupeter012001@outlook.com>
2025-07-16 10:47:13 +1000

    1.[BUG FIX]:test bug fix! m_size is not the most acc way to judge wether queue is empty! 2.[FORMATTING]

--- tests/test_queue_lockfree/test_queue_lockfree_multithread_push1_pop1.cc
+++ tests/test_queue_lockfree/test_queue_lockfree_multithread_push1_pop1.cc
@@ -1,35 +1,35 @@
-#include <queue_lockfree.hpp>
 #include <gtest/gtest.h>
+#include <queue_lockfree.hpp>
 #include <thread>
 
 #define NUMBER 2000000
 
 TEST(LockFreeRefQueueTest, MultiThreadPush1Pop1) {
-          concurrency::ConcurrentQueue<int> queue;
+  concurrency::ConcurrentQueue<int> queue;
 
-          auto producer = [&queue]() {
-                    for (std::size_t i = 0; i < NUMBER; ++i) {
-                              queue.push(i);
-                    }
-                    };
+  auto producer = [&queue]() {
+    for (std::size_t i = 0; i < NUMBER; ++i) {
+      queue.push(i);
+    }
+  };
 
-          auto consumer = [&queue]() {
-                    std::size_t popped = 0;
-                    while (popped < NUMBER) {
-                              auto val = queue.pop();
-                              if (val.has_value()) {
-                                        ++popped;
-                                        continue;
-                              }
-                              std::this_thread::yield();
-                    }
-                    };
+  auto consumer = [&queue]() {
+    std::size_t popped = 0;
+    while (popped < NUMBER) {
+      auto val = queue.pop();
+      if (val.has_value()) {
+        ++popped;
+        continue;
+      }
+      std::this_thread::yield();
+    }
+  };
 
-          std::thread th1(producer);
-          std::thread th2(consumer);
+  std::thread th1(producer);
+  std::thread th2(consumer);
 
-          th1.join();
-          th2.join();
+  th1.join();
+  th2.join();
 
-          EXPECT_EQ(queue.size(), 0);
+  EXPECT_TRUE(queue.empty());
 }
