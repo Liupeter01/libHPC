f4106b1201e75bc2b20ae3d1143b269907ff0055
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-07-17 22:30:16 +1000

    1.[UPDATE]:update test method with condition variable wait & notify_one for acc

--- tests/test_queue_lockfree/test_queue_lockfree_multithread_push2_pop4.cc
+++ tests/test_queue_lockfree/test_queue_lockfree_multithread_push2_pop4.cc
@@ -9,53 +9,50 @@
 #define TESTCOUNT 10
 #define NUMBER 2000000
 
-// TEST(LockFreeRefQueueTest, MultiThreadPush2Pop4) {
-//   for (std::size_t i = 0; i < TESTCOUNT; ++i) {
-//     concurrency::ConcurrentQueue<int> queue;
+ TEST(LockFreeRefQueueTest, MultiThreadPush2Pop4_Stable_WithNotify) {
+           for (std::size_t i = 0; i < TESTCOUNT; ++i) {
+                     concurrency::ConcurrentQueue<int> queue;
+                     std::mutex mtx;
+                     std::condition_variable cv;
+                     std::atomic<std::size_t> total_popped = 0;
+                     const std::size_t target = NUMBER * 2;
 
-//     std::mutex mtx;
-//     std::condition_variable cv;
-//     std::atomic<std::size_t> total_popped = 0;
-//     const std::size_t target = NUMBER * 2;
+                     // Producer
+                     auto producer = [&queue, &cv]() {
+                               for (std::size_t i = 0; i < NUMBER; ++i) {
+                                         queue.push(i);
+                                         cv.notify_one();  // notify a waiting consumer
+                               }
+                               };
 
-//     auto producer = [&]() {
-//       for (std::size_t i = 0; i < NUMBER; ++i) {
-//         queue.push(i);
-//         cv.notify_all();
-//       }
-//     };
+                     // Consumer
+                     auto consumer = [&]() {
+                               while (true) {
+                                         if (total_popped.load(std::memory_order_acquire) >= target)
+                                                   break;
 
-//     auto consumer = [&]() {
-//       std::unique_lock<std::mutex> lock(mtx, std::defer_lock);
-//       while (true) {
-//         if (total_popped.load() >= target)
-//           break;
+                                         std::unique_lock<std::mutex> lock(mtx);
+                                         cv.wait(lock, [&]() {
+                                                   return total_popped.load(std::memory_order_acquire) >= target || !queue.empty();
+                                                   });
 
-//         auto val = queue.pop();
-//         if (val.has_value()) {
-//           ++total_popped;
-//         } else {
-//           lock.lock();
-//           cv.wait_for(lock, std::chrono::microseconds(10));
-//           lock.unlock();
-//         }
-//       }
-//     };
+                                         while (auto val = queue.pop()) {
+                                                   ++total_popped;
+                                         }
+                               }
+                               };
 
-//     std::vector<std::thread> producers;
-//     std::vector<std::thread> consumers;
-//     for (int i = 0; i < 2; ++i) {
-//       producers.emplace_back(std::thread(producer));
-//     }
-//     for (int i = 0; i < 4; ++i) {
-//       consumers.emplace_back(std::thread(consumer));
-//     }
+                     std::vector<std::thread> producers ,consumers;
+                     for (int i = 0; i < 2; ++i) {
+                               producers.emplace_back(std::thread(producer));
+                     }
+                     for (int i = 0; i < 4; ++i) {
+                               consumers.emplace_back(std::thread(consumer));
+                     }
 
-//     for (auto &t : producers)
-//       t.join();
-//     for (auto &t : consumers)
-//       t.join();
+                     for (auto& p : producers) p.join();
+                     for (auto& c : consumers) c.join();
 
-//     EXPECT_TRUE(queue.empty());
-//   }
-// }
+                     EXPECT_TRUE(queue.empty());
+           }
+ }
