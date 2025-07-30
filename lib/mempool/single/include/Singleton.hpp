a88d690fd1eb5f56d41fc6e667ee0fc1e4640470
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-07-30 12:59:55 +1000

    1.[ADD]:add signleton paterrn to C++ style memory pool

---
+++ lib/mempool/single/include/Singleton.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#ifndef _SINGLETON_HPP_
+#define _SINGLETON_HPP_
+#include <memory>
+#include <mutex>
+
+template <typename _Ty> struct SingletonDeletor {
+  void operator()(_Ty *value) { delete value; }
+};
+
+template <typename _Ty> class Singleton {
+  static std::shared_ptr<_Ty> m_instance;
+
+  /*Once we delete move ctor then all copy ctor functions are deleted!*/
+  Singleton(Singleton &&) = delete;
+
+protected:
+  Singleton() = default;
+
+public:
+  ~Singleton() {}
+  static std::shared_ptr<_Ty> &Instance() {
+    static std::once_flag flag;
+    std::call_once(flag, [&]() {
+      m_instance = std::shared_ptr<_Ty>(new _Ty, SingletonDeletor<_Ty>());
+    });
+    return m_instance;
+  }
+};
+
+template <typename _Ty>
+std::shared_ptr<_Ty> Singleton<_Ty>::m_instance = nullptr;
+
+#endif //_SINGLETON_HPP_
