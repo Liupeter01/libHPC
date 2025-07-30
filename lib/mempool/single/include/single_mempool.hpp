efda6d8edea221b0defdb04bcefebb1c89e48583
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2025-07-30 13:02:29 +1000

    1.[NOT FINISHED]:C++ wrapper for single_mempool

---
+++ lib/mempool/single/include/single_mempool.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef _MEMPOOL_HPP_
+#define _MEMPOOL_HPP_
+extern "C" {
+#include <single_mempool_impl.h>
+}
+#include <Singleton.hpp>
+
+namespace mempool {
+          class SingleMemoryPool : public Singleton<SingleMemoryPool> {
+                    friend class Singleton<SingleMemoryPool>;
+          public:
+                    ~SingleMemoryPool() {}
+          private:
+                    SingleMemoryPool() {}
+          };
+}
+
+#endif //_MEMPOOL_HPP_
