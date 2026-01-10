badfac719cb72796b659f4b03f4dc72b223f9d64
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2026-01-10 20:37:44 +1100

    1.[MOVE]:move namespace v1 to cuda_radix_sort_v1 2.[ADD]:add radix_sort_v3, we use a while-loop to control reduction operation by using kernel_global_reduce_from_local_v3

---
+++ lib/gpu/radix_gpu/include/cuda_radix_sort_v1.cuh
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef _CUDA_RADIX_SORT_V1_CUH_
+#define _CUDA_RADIX_SORT_V1_CUH_
+#include <common.hpp>
+
+namespace sort {
+          namespace gpu {
+                    namespace radix {
+                              namespace details {
+                                        namespace v1 {
+
+                                                  void __radix_sort_v1(
+                                                            std::vector<uint32_t, CudaAllocator<uint32_t, CudaMemManaged>>& input);
+                                        } // namespace v1
+                              } // namespace details
+                    } // namespace radix
+          } // namespace gpu
+} // namespace sort
+
+#endif
---
+++ lib/gpu/radix_gpu/include/cuda_radix_sort_v2.cuh
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef _CUDA_RADIX_SORT_V2_CUH_
+#define _CUDA_RADIX_SORT_V2_CUH_
+#include <common.hpp>
+
+namespace sort {
+          namespace gpu {
+                    namespace radix {
+                              namespace details {
+                                        namespace v2 {
+
+                                                  void __radix_sort_v2(
+                                                            std::vector<uint32_t, CudaAllocator<uint32_t, CudaMemManaged>>& input);
+
+                                        } // namespace v2
+                              } // namespace details
+                    } // namespace radix
+          } // namespace gpu
+} // namespace sort
+
+#endif
