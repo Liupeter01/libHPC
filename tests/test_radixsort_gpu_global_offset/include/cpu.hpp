50176d815637ab984d6928fe8206a5c7a3df3eba
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2026-01-14 13:15:01 +1100

    1.[ADD]:add test_radixsort_gpu_* as unit test

---
+++ tests/test_radixsort_gpu_global_offset/include/cpu.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef _CPU_HPP_
+#define _CPU_HPP_
+#include <vector>
+
+void cpu_local_count_ref(const std::vector<uint32_t> &in, size_t numBlocks,
+                         std::vector<uint32_t> &out_local_count);
+void cpu_local_offset_ref(std::vector<uint32_t> &local, size_t numBlocks);
+void cpu_global_base_ref(const std::vector<uint32_t> &in,
+                         std::vector<uint32_t> &base);
+
+#endif //_CPU_HPP_
