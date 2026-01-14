92ed114a27df0fa3d2ce0d7e3a58732700d507ed
PEIHENG(Jonathan) LIU <Liupeter012001@outlook.com>
2026-01-14 13:19:46 +1100

    1.[ADD]:add ceil div and align function

--- lib/gpu/radix_gpu/include/common.hpp
+++ lib/gpu/radix_gpu/include/common.hpp
@@ -10,6 +10,7 @@
 #include <nvtx3/nvToolsExt.h>
 #include <thrust/execution_policy.h>
 #include <thrust/scan.h>
+#include <thrust/device_vector.h>
 #include <type_traits>
 #include <vector>
 
@@ -19,6 +20,13 @@ namespace radix {
 namespace details {
 static constexpr inline std::intptr_t constexpr_log2(std::intptr_t n) {
   return (n < 2) ? 0 : 1 + constexpr_log2(n >> 1);
+}
+
+static inline uint32_t div_up_u32(uint32_t a, uint32_t b) {
+          return (a + b - 1u) / b;
+}
+static inline uint32_t align_up_u32(uint32_t a, uint32_t align) {
+          return ((a + align - 1u) / align) * align;
 }
 } // namespace details
 } // namespace radix
